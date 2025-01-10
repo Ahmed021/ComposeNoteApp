f3f8af926080bd6ce4892197fcd8fcca59f95873
ahmedonibiyo <ahmedabiodun4@gmail.com>
2025-01-10 20:53:48 +0100

    Setup navigation in MainActivity

--- app/src/main/java/com/ahmedonibiyo/noteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/ahmedonibiyo/noteapp/feature_note/presentation/MainActivity.kt
@@ -1,47 +1,162 @@
 package com.ahmedonibiyo.noteapp.feature_note.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ahmedonibiyo.noteapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.ahmedonibiyo.noteapp.feature_note.presentation.notes.NotesScreen
+import com.ahmedonibiyo.noteapp.feature_note.presentation.sign_in.GoogleAuthClientUi
+import com.ahmedonibiyo.noteapp.feature_note.presentation.sign_in.SignInScreen
+import com.ahmedonibiyo.noteapp.feature_note.presentation.sign_in.SignInViewModel
+import com.ahmedonibiyo.noteapp.feature_note.presentation.util.Screen
 import com.ahmedonibiyo.noteapp.ui.theme.NoteAppTheme
+import com.google.android.gms.auth.api.identity.Identity
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
+
+    private val googleAuthClientUi by lazy {
+        GoogleAuthClientUi(
+            context = applicationContext,
+            oneTapClient = Identity.getSignInClient(applicationContext)
+        )
+    }
+
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             NoteAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.SignInScreen.route
+                    ) {
+                        composable(route = Screen.SignInScreen.route) {
+                            val viewModel = viewModel<SignInViewModel>()
+                            val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            LaunchedEffect(key1 = Unit) {
+                                if (googleAuthClientUi.getSignedInUser() != null) {
+                                    navController.navigate(Screen.ProfileScreen.route)
+                                }
+                            }
+
+                            val launcher = rememberLauncherForActivityResult(
+                                contract = ActivityResultContracts.StartIntentSenderForResult(),
+                                onResult = {
+                                    if (it.resultCode == RESULT_OK) {
+                                        lifecycleScope.launch {
+                                            val signInResult = googleAuthClientUi.signInWithIntent(
+                                                intent = it.data ?: return@launch
+                                            )
+                                            viewModel.onSignInResult(signInResult)
+                                        }
+                                    }
+                                }
+                            )
+
+                            LaunchedEffect(key1 = state.isSignInSuccessful) {
+                                if (state.isSignInSuccessful) {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Sign in successful",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+
+                                navController.navigate(Screen.ProfileScreen.route)
+                                viewModel.resetState()
+                            }
+
+                            SignInScreen(
+                                state = state,
+                                onSignInClick = {
+                                    lifecycleScope.launch {
+                                        val signInIntentSender = googleAuthClientUi.signIn()
+                                        launcher.launch(
+                                            IntentSenderRequest.Builder(
+                                                signInIntentSender ?: return@launch
+                                            ).build()
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                        composable(route = Screen.ProfileScreen.route) {
+                            ProfileScreen(
+                                userData = googleAuthClientUi.getSignedInUser(),
+                                onProceedToNotesClick = {
+                                    navController.navigate(Screen.NotesScreen.route)
+                                },
+                                onSignOutClick = {
+                                    lifecycleScope.launch {
+                                        googleAuthClientUi.signOut()
+                                        Toast.makeText(
+                                            applicationContext,
+                                            "Signed out",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+
+                                        navController.popBackStack()
+                                    }
+                                }
+                            )
+                        }
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route +
+                                    "?noteId={notedId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteAppTheme {
-        Greeting("Android")
     }
 }
